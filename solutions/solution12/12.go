a41a750dd12459ff29aced53c78b30a163e9f404
Ali Ozgur <aliozgur79@gmail.com>
2019-12-31 11:58:34 +0300

    Day 12

--- solutions/solution12/12.go
+++ solutions/solution12/12.go
@@ -16,7 +16,7 @@ const puzzle = `
 <x=-8, y=0, z=4>
 `
 
-func readInput(puzzle string) (result []Moon) {
+func readInput(puzzle string) (result []moon) {
 	var lines = strings.NewReader(puzzle)
 	scanner := bufio.NewScanner(lines)
 	for scanner.Scan() {
@@ -24,8 +24,8 @@ func readInput(puzzle string) (result []Moon) {
 		if line == "" {
 			continue
 		}
-		moon := Moon{}
-		fmt.Sscanf(line, "<x=%d, y=%d, z=%d>", &moon.Position[0], &moon.Position[1], &moon.Position[2])
+		moon := moon{}
+		fmt.Sscanf(line, "<x=%d, y=%d, z=%d>", &moon.position[0], &moon.position[1], &moon.position[2])
 		result = append(result, moon)
 	}
 
@@ -35,28 +35,27 @@ func readInput(puzzle string) (result []Moon) {
 	return
 }
 
-type Point int
+type point int
 
-type Moon struct {
-	Name     string
-	Gravity  [3]Point
-	Position [3]Point
-	Velocity [3]Point
+type moon struct {
+	tempGravity [3]point
+	position    [3]point
+	velocity    [3]point
 }
 
-func (moon *Moon) String() string {
-	return fmt.Sprintf("%s pos=<x=%d,y=%d,z=%d>, vel=<x=%d,y=%d,z=%d>",
-		moon.Name,
-		moon.Position[0],
-		moon.Position[1],
-		moon.Position[2],
-		moon.Velocity[0],
-		moon.Velocity[1],
-		moon.Velocity[2],
+func (moon *moon) String() string {
+	return fmt.Sprintf("pos=<x=%d,y=%d,z=%d>, vel=<x=%d,y=%d,z=%d>",
+		moon.position[0],
+		moon.position[1],
+		moon.position[2],
+		moon.velocity[0],
+		moon.velocity[1],
+		moon.velocity[2],
 	)
 }
 
 func Part1() {
+	fmt.Println("Part 1 starting...")
 	moons := readInput(puzzle)
 	for step := 1; step <= 1000; step++ {
 		for i := 0; i < len(moons); i++ {
@@ -67,35 +66,37 @@ func Part1() {
 				for k := 0; k < 3; k++ {
 					m1 := &moons[i]
 					m2 := &moons[j]
-					g1, g2 := comparePoints(m1.Position[k], m2.Position[k])
-					m1.Gravity[k] += g1
-					m2.Gravity[k] += g2
+					g1, g2 := comparePoints(m1.position[k], m2.position[k])
+					m1.tempGravity[k] += g1
+					m2.tempGravity[k] += g2
 				}
 			}
 			process(&moons[i])
 		}
+		printMoons(moons, step)
 	}
 
 	totalEnergy := 0
 	for i := 0; i < len(moons); i++ {
 		totalEnergy += calculateEnergy(moons[i])
 	}
-
-	fmt.Println("Total Energy:", totalEnergy)
+	fmt.Println("Part 1 DONE (Total Energy):", totalEnergy)
 }
 
 func Part2() {
+	fmt.Println("Part 2 starting...")
 	moons := readInput(puzzle)
-	initialMoons := readInput(puzzle)
+	//initialMoons := readInput(puzzle)
 	var initialX, initialY, initialZ string
-	for i:=0;i<len(moons);i++{
-		initialX += fmt.Sprint(moons[i].Position[0])
-		initialY += fmt.Sprint(moons[i].Position[1])
-		initialZ += fmt.Sprint(moons[i].Position[2])
+	for i := 0; i < len(moons); i++ {
+		initialX += fmt.Sprint(moons[i].position[0])
+		initialY += fmt.Sprint(moons[i].position[1])
+		initialZ += fmt.Sprint(moons[i].position[2])
 	}
 
 	var freqX, freqY, freqZ int
-	for step := 2; ;step++ {
+
+	for step := 2; ; step++ {
 		var stateX, stateY, stateZ string
 		for i := 0; i < len(moons); i++ {
 			for j := i; j < len(moons); j++ {
@@ -105,15 +106,15 @@ func Part2() {
 				for k := 0; k < 3; k++ {
 					m1 := &moons[i]
 					m2 := &moons[j]
-					g1, g2 := comparePoints(m1.Position[k], m2.Position[k])
-					m1.Gravity[k] += g1
-					m2.Gravity[k] += g2
+					g1, g2 := comparePoints(m1.position[k], m2.position[k])
+					m1.tempGravity[k] += g1
+					m2.tempGravity[k] += g2
 				}
 			}
 			process(&moons[i])
-			stateX += fmt.Sprint(moons[i].Position[0])
-			stateY += fmt.Sprint(moons[i].Position[1])
-			stateZ += fmt.Sprint(moons[i].Position[2])
+			stateX += fmt.Sprint(moons[i].position[0])
+			stateY += fmt.Sprint(moons[i].position[1])
+			stateZ += fmt.Sprint(moons[i].position[2])
 		}
 
 		if stateX == initialX && freqX == 0 {
@@ -126,7 +127,9 @@ func Part2() {
 			freqZ = step
 		}
 
+		// All dimensions are aligned at least once
 		if freqX != 0 && freqY != 0 && freqZ != 0 {
+			fmt.Println("Part 2: All dimension are aligned at leas once after ",step, " steps")
 			break
 		}
 	}
@@ -138,16 +141,18 @@ func Part2() {
 	if freqZ > maxFreq {
 		maxFreq = freqZ
 	}
-	var mcmFreqs int = maxFreq
 
+	// Now lets brute force and increment by max freq until all dimensions are in line
+	fmt.Println("Part 2: Trying to find the step when all dimensions will be inline...")
+	mcmFreqs := maxFreq
 	for mcmFreqs%freqX != 0 || mcmFreqs%freqY != 0 || mcmFreqs%freqZ != 0 {
 		mcmFreqs += maxFreq
 	}
-	fmt.Println("Part 2 (Steps until initial position):", mcmFreqs)
+	fmt.Println("Part 2 DONE (Steps until initial position):", mcmFreqs)
 
 }
 
-func printMoons(moons []Moon, step int) {
+func printMoons(moons []moon, step int) {
 	if !utils.DEBUG {
 		return
 	}
@@ -156,57 +161,58 @@ func printMoons(moons []Moon, step int) {
 		fmt.Println(moons[i].String())
 	}
 }
-func comparePoints(p1 Point, p2 Point) (change1, change2 Point) {
+
+func comparePoints(p1 point, p2 point) (change1, change2 point) {
 	if p1 < p2 {
-		change1 = Point(1)
-		change2 = Point(-1)
+		change1 = point(1)
+		change2 = point(-1)
 	} else if p2 < p1 {
-		change1 = Point(-1)
-		change2 = Point(1)
+		change1 = point(-1)
+		change2 = point(1)
 	} else {
-		change1 = Point(0)
-		change2 = Point(0)
+		change1 = point(0)
+		change2 = point(0)
 	}
 	return
 }
 
-func applyGravity(m *Moon) {
+func applyGravity(m *moon) {
 	for i := 0; i < 3; i++ {
-		m.Velocity[i] += m.Gravity[i]
-		m.Gravity[i] = 0
+		m.velocity[i] += m.tempGravity[i]
+		m.tempGravity[i] = 0
 	}
 }
 
-func applyVelocity(m *Moon) {
+func applyVelocity(m *moon) {
 	for i := 0; i < 3; i++ {
-		m.Position[i] += m.Velocity[i]
+		m.position[i] += m.velocity[i]
 	}
 }
 
-func calculateEnergy(m Moon) (result int) {
+func calculateEnergy(m moon) (result int) {
 	pot := 0
 	kin := 0
 
 	for i := 0; i < 3; i++ {
-		pot += int(math.Abs(float64(m.Position[i])))
-		kin += int(math.Abs(float64(m.Velocity[i])))
+		pot += int(math.Abs(float64(m.position[i])))
+		kin += int(math.Abs(float64(m.velocity[i])))
 	}
 	result = pot * kin
 	return
 }
 
-func process(m *Moon) {
+func process(m *moon) {
 	applyGravity(m)
 	applyVelocity(m)
 }
 
-func compareStates(initial []Moon, current []Moon) (result bool) {
+func compareStates(initial []moon, current []moon) (result bool) {
 	result = true
 	exit := false
 	for i := 0; i < len(initial); i++ {
 		for k := 0; k < 3; k++ {
-			v1, v2 := comparePoints(initial[i].Velocity[k], current[i].Velocity[k])
-			p1, p2 := comparePoints(initial[i].Position[k], current[i].Position[k])
+			v1, v2 := comparePoints(initial[i].velocity[k], current[i].velocity[k])
+			p1, p2 := comparePoints(initial[i].position[k], current[i].position[k])
 			if v1 != 0 || v2 != 0 || p1 != 0 || p2 != 0 {
 				result = false
 				exit = true
